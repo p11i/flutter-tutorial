8393a85d2054ba32cf14afd8d69efe31b21b281c
Pouya Hosseyni <pouyahosseyni7@gmail.com>
2018-12-02 14:54:42 +0330

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlutterTutorial());
+
+class FlutterTutorial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
